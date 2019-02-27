be7f155ca77192f0225c2ba308c4bbd9ab71e377
Thanh <thanhqd1990@gmail.com>
2019-02-26 23:47:39 -0600

    Added validation for url parameter: Numbers only

--- pages/users/_id/index.vue
+++ pages/users/_id/index.vue
@@ -1,3 +1,12 @@
 <template>
     <h1>Dynamic page: {{ $route.params.id }}</h1>
 </template>
+
+<script>
+export default {
+    validate (data) {
+        return /^\d+$/.test(data.params.id)
+    }
+}
+</script>
+
