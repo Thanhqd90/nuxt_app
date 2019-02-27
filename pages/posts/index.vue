b95d991490466c773e38656a61028912c6a47269
Thanh <thanhqd1990@gmail.com>
2019-02-27 08:29:31 -0600

    Added some styling and dynamic route

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -1,11 +1,31 @@
 <template>
-  <div>
-    <h1>Posts Page</h1>
+  <div class="posts-page">
+    <input type="text" v-model="postId">
+    <button @click="onLoadUser">Find Post</button>
   </div>
 </template>
 
 <script>
+export default {
+  data() {
+    return {
+      postId: ""
+    };
+  },
+  methods: {
+    onLoadUser() {
+      this.$router.push("/posts/" + this.postId);
+    }
+  }
+};
 </script>
 
-<style>
+<style scoped>
+.posts-page {
+  text-align: center;
+  padding: 30px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 </style>
